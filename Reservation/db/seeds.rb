da49b48074326ada095c9ef4d6d3035186c0ff69
karlley <karlley.jp@gmail.com>
2023-04-01 05:56:56 +0900

    20230401

--- Reservation/db/seeds.rb
+++ Reservation/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do |n|
+  Room.create!(
+    name: "部屋番号: #{n}",
+    number: 4
+  )
+end
