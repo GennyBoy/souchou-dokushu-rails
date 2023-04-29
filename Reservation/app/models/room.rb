3504650145120a6704ca694ff4644cea27e00efe
karlley <karlley.jp@gmail.com>
2023-04-30 05:53:30 +0900

    20230430

--- Reservation/app/models/room.rb
+++ Reservation/app/models/room.rb
@@ -1,2 +1,11 @@
 class Room < ApplicationRecord
+  validates :name,
+    presence: true,
+    length: { maximum: 30 }
+  validates :place,
+    presence: true,
+    inclusion: [ "東京", "大阪", "福岡", "札幌", "仙台", "名古屋", "金沢" ]
+  validates :number,
+    presence: true,
+    numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 30 }
 end
