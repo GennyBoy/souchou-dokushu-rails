67422dcfd9dbd08dbda3cd3c4d90b681a808d43d
hiromisugie <lozz.gggg@gmail.com>
2023-05-13 06:08:05 +0900

    20230513

--- Reservation/app/models/room.rb
+++ Reservation/app/models/room.rb
@@ -1,11 +1,20 @@
 class Room < ApplicationRecord
   validates :name,
-    presence: true,
-    length: { maximum: 30 }
+    presence: { message: "会議名の入力は必須です" },
+    length: { maximum: 30, message: "会議名は30文字以内で入力してください" },
+    format: { with: /.*#\d{2}\z/, message: "会議名は「#数字」で終わる必要があります" }
   validates :place,
     presence: true,
     inclusion: [ "東京", "大阪", "福岡", "札幌", "仙台", "名古屋", "金沢" ]
   validates :number,
     presence: true,
     numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 30 }
+  validate :number_multiple_of_5
+
+  private
+  def number_multiple_of_5
+    unless self.number % 5 == 0
+      errors.add(:number, "は5の倍数で入力してください")
+    end
+  end
 end
