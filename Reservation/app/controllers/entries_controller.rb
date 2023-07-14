c267fdf512c07e5716ca936e9e9b85e355378152
karlley <karlley.jp@gmail.com>
2023-07-15 06:01:18 +0900

    予約登録機能を追加

---
+++ Reservation/app/controllers/entries_controller.rb
@@ -0,0 +1,16 @@
+class EntriesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def confirm
+  end
+end
--- Reservation/config/routes.rb
+++ Reservation/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get '/', to: 'rooms#index'
+  root to: 'rooms#index'
   get 'top/index'
   resources :rooms
+  resources :entries, only: %i[new create destroy index], path: :rentals
+  post 'rentals/confirm', to: 'entries#confirm'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
