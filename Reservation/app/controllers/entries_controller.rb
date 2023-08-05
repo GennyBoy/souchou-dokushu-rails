6afa21daa931f93e3b915348afe93172e8b65e78
GennyBoy <tassie2856@gmail.com>
2023-08-06 06:28:14 +0900

    色々やった

--- Reservation/app/controllers/entries_controller.rb
+++ Reservation/app/controllers/entries_controller.rb
@@ -1,16 +1,39 @@
 class EntriesController < ApplicationController
+  before_action :set_entry, only: :destroy
+
   def new
+    @entry = Entry.new
   end
 
   def index
+    @entries = Entry.all
   end
 
   def create
+    @entry = Entry.new(entry_params)
+
+    if @entry.save
+      redirect_to entry_url(@entry), notice: "Entry was successfully created."
+    else
+      render :new
+    end
   end
 
   def destroy
+    @entry.destroy
+
+    redirect_to entries_url, notice: "Entry was successfully destroyed."
   end
 
   def confirm
+  end
+
+  private
+  def set_entry
+    @entry = Entry.find(params[:id])
+  end
+
+  def entry_params
+    params.require(:entry).permit(:user_name, :user_email, :reserved_date, :usage_time, :room_id, :people)
   end
 end
