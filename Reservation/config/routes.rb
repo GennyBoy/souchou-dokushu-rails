402a82408e10ac74aaee4dc790b34ea947652049
miyanew <dev38_shin@yahoo.co.jp>
2023-09-16 06:04:58 +0900

    20230916

--- Reservation/config/routes.rb
+++ Reservation/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'rooms#index'
   get 'top/index'
   resources :rooms
-  resources :entries, only: %i[new create destroy index], path: :rentals
+  resources :entries, only: %i[new create destroy index]
   post 'rentals/confirm', to: 'entries#confirm'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
