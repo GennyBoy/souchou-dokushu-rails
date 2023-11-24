4ff3727429c843a681a8d3b4f300554ff92e890d
hiromisugie <lozz.gggg@gmail.com>
2023-11-25 06:01:56 +0900

    20231125

--- Reservation/app/controllers/auths_controller.rb
+++ Reservation/app/controllers/auths_controller.rb
@@ -4,8 +4,28 @@ class AuthsController < ApplicationController
   end
 
   def create
+    @auth = Auth.new(auth_params)
+
+    if @auth.valid?
+      user = User.find_by(email: params[:auth][:email])
+      if user && user.password == params[:auth][:password]
+        session[:user_id] = user.id
+        redirect_to root_path
+      else
+        redirect_to new_auths_path
+      end
+    else
+      render :new
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to new_auths_path
   end
+
+  private
+    def auth_params
+      params.require(:auth).permit(:email, :password)
+    end
 end
