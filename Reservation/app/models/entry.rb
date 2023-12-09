db2efe1f1def64d0ae636cb0296b6449cf6c06f8
hiromisugie <lozz.gggg@gmail.com>
2023-12-10 06:32:30 +0900

    entryとuserを関連づけた

--- Reservation/app/models/entry.rb
+++ Reservation/app/models/entry.rb
@@ -1,11 +1,11 @@
 class Entry < ApplicationRecord
   belongs_to :room
+  belongs_to :user
 
   scope :least_entries,->(base_date) {
     where("reserved_date >= ? and reserved_date <= ?",
     base_date.to_date - 7.days, base_date.to_date + 7.days )
   }
 
-  validates :user_name, :user_email, :reserved_date, :usage_time, presence: true
-  validates :user_email, uniqueness: true
+  validates :user_id, :reserved_date, :usage_time, presence: true
 end
--- Reservation/app/models/user.rb
+++ Reservation/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+has_many :entries
+
   validates :name, :email, :password, presence: true
   validates :email, uniqueness: true
 end
