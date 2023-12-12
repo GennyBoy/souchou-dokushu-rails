42d9e60c0bdd2a3b173d18e7d341b97da468c36c
GennyBoy <tassie2856@gmail.com>
2023-12-12 16:56:02 +0900

    コメントを削除

--- Reservation/app/controllers/entries_controller.rb
+++ Reservation/app/controllers/entries_controller.rb
@@ -2,9 +2,6 @@ class EntriesController < ApplicationController
   before_action :set_entry, only: :destroy
 
   def new
-    # app/views/entries/_form.html.erb で `Create Entry` を押すと confirm が呼ばれる。
-    # もともとやっていたやり方だと create で user_id をセットしていたので、 confirm が呼ばれた時点では、 user が紐づけられておらずエラーになる。
-    # new で user を紐づけてあげて、 view から hidden parameter で送るという、 room_id でも使っているやり方を適用して解決した
     @entry = Entry.new(room_id: params[:room_id], user_id: current_user.id)
   end
 
@@ -14,10 +11,6 @@ class EntriesController < ApplicationController
 
   def create
     @entry = Entry.new(entry_params)
-    # 画面遷移的に new -> confirm -> create の順で呼ばれる
-    # 最初は下記のようにこのタイミングで user を紐づけていた。
-    # このやり方だとconfirmメソッドが呼ばれたタイミングで params に user_id がなく落ちてしまう
-    # @entry = Entry.new(room_id: params[:room_id], user_id: loggedin_user.id, reserved_date: params[:reserved_date], usage_time: params[:usage_time], people: params[:people])
 
     if params[:back]
       render :new
@@ -39,11 +32,8 @@ class EntriesController < ApplicationController
     head :no_content
   end
 
-  # app/views/entries/_form.html.erb で `Create Entry` を押すと  createの前にが呼ばれる。
-  # app/views/entries/confirm.html.erb でもう一度 `Create Entry` を押すと、 create が呼ばれる
   def confirm
     @entry = Entry.new(entry_params)
-    # createでuser紐付けを行なっていたときは、下記の処理で落ちていた。
     @entry.valid?
   end
 
@@ -54,7 +44,6 @@ class EntriesController < ApplicationController
   end
 
   def entry_params
-    # formから user_id を送るようにしたのでパラメータとして受け付けるようにする
     params.require(:entry).permit(:reserved_date, :usage_time, :room_id, :people, :user_id)
   end
 end
