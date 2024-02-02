020038ca5c0bea737bdd8227434e1b065fc55b0a
hiromisugie <lozz.gggg@gmail.com>
2024-02-03 06:08:35 +0900

    fixtureデータ作成、モデルテスト（バリデーション）

--- Reservation/test/models/room_test.rb
+++ Reservation/test/models/room_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "名前が空欄の時にメッセージを出す" do
+    room1 = rooms(:room1)
+    room1.name = ""
+    room1.valid?
+    assert room1.errors.messages[:name].include?("会議名の入力は必須です")
+  end
+
+  test "場所が横浜の時にメッセージを出す" do
+    room1 = rooms(:room1)
+    room1.place = "横浜"
+    room1.valid?
+    p room1.errors.messages
+    assert room1.errors.messages[:place].include?("は一覧にありません")
+  end
+
+  test "人数が5の倍数でない時にメッセージを出す" do
+    room1 = rooms(:room1)
+    room1.number = 6
+    assert !room1.valid?
+    assert room1.errors.messages[:number].include?("は5の倍数で入力してください")
+  end
 end
