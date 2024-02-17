c38b2591af2332ef9011586cefba8350ad35aadd
hiromisugie <lozz.gggg@gmail.com>
2024-02-18 06:17:50 +0900

    2024/02/18 最終回ここに来たり！！！！！！

--- Reservation/test/controllers/entries_controller_test.rb
+++ Reservation/test/controllers/entries_controller_test.rb
@@ -1,14 +1,26 @@
 require 'test_helper'
 
 class EntriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get entries_new_url
-    assert_response :success
+
+  setup do
+    @user = users(:user1)
+    valid_params = {"auth"=>{"email"=>"sample1@example.com", "password"=> "sample1"}}
+    post auths_url(valid_params)
+    @room = rooms(:room1)
   end
 
-  test "should get index" do
-    get entries_index_url
+  test "予約登録" do
+    get new_room_entry_url(@room)
     assert_response :success
+
+    post room_entries_url, params: { entry: { room_id: @room.id, user_id: @user.id, reserved_date: Date.today, usage_time: 1, people: 5}}
+    assert_equal flash[:notice], "Entry was successfully created."
   end
 
+  test "予約削除" do
+    assert_difference "Entry.count", -1 do
+      delete entry_url(Entry.last)
+      assert_response :success
+    end
+  end
 end
